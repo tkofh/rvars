a0626b69bd8375973512d5f8544c9ffc02c07fd8
Tim Morris <timthewebguy@gmail.com>
2023-02-01 10:16:33 -0500

    fix(@rvars/tailwind-plugin): use initial rather than empty string

--- packages/tailwind-plugin/src/plugin.ts
+++ packages/tailwind-plugin/src/plugin.ts
@@ -18,7 +18,7 @@ const plugin = <TAdditionalConfigKeys extends string = never>(
 
           const result: Record<string, string | Record<string, string>> = {}
 
-          result[`--${varName}-${baseBreakpointName}`] = ` `
+          result[`--${varName}-${baseBreakpointName}`] = `initial`
           result[`--i-${varName}-${baseBreakpointName}`] = defaultValue
             ? `var(--${varName}-${baseBreakpointName}, ${defaultValue})`
             : `var(--${varName}-${baseBreakpointName})`
@@ -26,7 +26,7 @@ const plugin = <TAdditionalConfigKeys extends string = never>(
           result[`--${varName}`] = `var(--i-${varName}-${baseBreakpointName})`
 
           for (const [index, screen] of screens.entries()) {
-            result[`--${varName}-${screen.name}`] = ` `
+            result[`--${varName}-${screen.name}`] = `inital`
             result[`--i-${varName}-${screen.name}`] = `var(--${varName}-${
               screen.name
             }, var(--i-${varName}-${index === 0 ? baseBreakpointName : screens[index - 1].name}))`
